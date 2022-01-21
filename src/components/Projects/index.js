8d53de1cd1641417d23ab219ec3c1cfabf8ceaca
Calvin Donner <calvinjdonner@gmail.com>
2022-01-20 20:15:47 -0700

    updated color variables and added colors to project webpage dots

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,9 +4,9 @@ const Projects = ({ img, link }) => {
     return (
         <div className="p">
             <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
+                <div className="p-circle1"></div>
+                <div className="p-circle2"></div>
+                <div className="p-circle3"></div>
             </div>
             <a href={link} target="_blank" rel="noreferrer">
                 <img src={img} alt="" className="p-img"></img>
