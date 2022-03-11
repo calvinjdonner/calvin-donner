cedf18ef1b0503ac2844d4e2f680bf56eb5db7c3
Calvin Donner <calvinjdonner@gmail.com>
2022-03-11 14:17:48 -0700

    made changes to lists

--- src/components/Lists/index.js
+++ src/components/Lists/index.js
@@ -3,8 +3,127 @@ import React from 'react'
 const Lists = () => {
 
     return (
-        <div>
-            HERE IS A LIST OF MY FAVORITE THINGS - GET TO KNOW ME!
+        <div className='flexcontainer'>
+            <h3 className='innerbox'>Favorite Albums
+                <div className='boxlist'>
+                    <ul>
+                        <li>
+                            The Suburbs - Arcade Fire
+                        </li>
+                        <li>
+                            Viva La Vida or Death and All of His Friends - Coldplay
+                        </li>
+                        <li>
+                            Random Access Memories - Daft Punk
+                        </li>
+                        <li>
+                            Fleet Foxes - Fleet Foxes
+                        </li>
+                        <li>
+                            Babel - Mumford & Sons
+                        </li>
+                        <li>
+                            Dark Side of The Moon - Pink Floyd
+                        </li>
+                        <li>
+                            My Beautiful Dark Twisted Fantasy - Kanye West
+                        </li>
+                    </ul>
+                </div>
+            </h3>
+            <h3 className='innerbox'>Favorite Movies
+                <div className='boxlist'>
+                    <ul>
+                        <li>
+                            True Grit (2010)
+                        </li>
+                        <li>
+                            The Social Network (2010)
+                        </li>
+                        <li>
+                            The Big Short (2015)
+                        </li>
+                        <li>
+                            Chinatown (1974)
+                        </li>
+                        <li>
+                            Indiana Jones and the Last Crusade (1989)
+                        </li>
+                        <li>
+                            Lord of the Rings: The Return of the King (2003)
+                        </li>
+                        <li>
+                            Back To The Future (1985)
+                        </li>
+                        <li>
+                            Saving Private Ryan (1998)
+                        </li>
+                        <li>
+                            Spider-Man: Into the Spider-Verse (2018)
+                        </li>
+                        <li>
+                            Midnight in Paris (2011)
+                        </li>
+                    </ul>
+                </div>
+            </h3>
+            <h3 className='innerbox'>Favorite Video Games
+                <div className='boxlist'>
+                    <ul>
+                        <li>
+                            Red Dead Redemption 2
+                        </li>
+                        <li>
+                            Half-Life 2
+                        </li>
+                        <li>
+                            Uncharted Series
+                        </li>
+                        <li>
+                            God of War
+                        </li>
+                        <li>
+                            Call of Duty
+                        </li>
+                        <li>
+                            Battlefield 1
+                        </li>
+                        <li>
+                            Pokémon Yellow
+                        </li>
+                        <li>
+                            Fallout 4
+                        </li>
+                        <li>
+                            Assassin's Creed II
+                        </li>
+                        <li>
+                            Tetris
+                        </li>
+                    </ul>
+                </div>
+            </h3>
+            <h3 className='innerbox'>Books I'm Reading
+                <div className='boxlist'>
+                    <ul>
+                        <li>
+                            Hitler's Last Hostages - Mary M. Lane
+                        </li>
+                        <li>
+                            Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones - James Clear
+                        </li>
+                        <li>
+                            Nine Innings - Daniel Okrent
+                        </li>
+                        <li>
+                            Ready Player Two - Ernest Cline
+                        </li>
+                        <li>
+                            The Third Reich at War - Richard J. Evans
+                        </li>
+                    </ul>
+                </div>
+            </h3>
         </div>
     )
 }
